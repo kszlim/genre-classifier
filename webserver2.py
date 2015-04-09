94ed60291037191a650319dcae00b068fa2cd625
Kevin Lim <kszlim@gmail.com>
2015-04-08 20:12:12 -0700

    update

--- webserver2.py
+++ webserver2.py
@@ -15,70 +15,10 @@ cors = CORS(app)
 
 predictor = create_model()
 
-button = {
-	1 :{'name':'item x','state':"False"},
-	2 :{'name':'item y','state':"False"},
-	3 :{'name':'item z','state':"False"},
-	4 :{'name':'item u','state':"False"}
-	}
 @app.route("/")
 def main():
-	
-	templateData = {
-		'button' : button
-	}
-	return render_template('main.html', **templateData)
+	return render_template('main.html')
 
-@app.route("/1/<state>")
-def button1(state):
-	optionName = button[1]['name']
-	if button[1]['state'] == "True":
-		button[1]['state'] = "False"
-		message = optionName + " is running."
-	elif button[1]['state'] == "False":
-		button[1]['state'] = "True"
-		message = optionName + " is closed."
-	templateData = {
-		'button' : button,
-		'message' : message
-	}
-	return render_template('main.html', **templateData)
-
-
-@app.route("/2/<state>")
-def button2(state):
-	optionName = button[2]['name']
-	if button[2]['state'] == "True":
-		button[2]['state'] = "False"
-		message = optionName + " is running."
-	elif button[2]['state'] == "False":
-		button[2]['state'] = "True"
-		message = optionName + " is closed."
-	templateData = {
-		'button' : button,
-		'message' : message
-	}
-	return render_template('main.html', **templateData)
-
-@app.route("/3/<state>")
-def button3(state):
-	optionName = button[3]['name']
-	if button[3]['state'] == "True":
-		button[3]['state'] = "False"
-		message = optionName + " is running."
-	elif button[3]['state'] == "False":
-		button[3]['state'] = "True"
-		message = optionName + " is closed."
-	templateData = {
-		'button' : button,
-		'message' : message
-	}
-	return render_template('main.html', **templateData)
-
-
-@app.route("/page1")
-def page_one():
-	return render_template('page_one.html')
 
 @app.route("/musicplayer")
 def player():
